9e59c4e495047778b88fa1683b380f0fdea6d710
Asanka <jayawardanajapan@gmail.com>
2020-09-26 15:04:24 +1000

    change setting

--- reservationhotel/settings.py
+++ reservationhotel/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't)_jqlcfwd+lg*$2f!8+11@5ta0w=lv7zzqdh=$7t5q17-cpi9'
-
+# SECRET_KEY = 't)_jqlcfwd+lg*$2f!8+11@5ta0w=lv7zzqdh=$7t5q17-cpi9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hetelreservation.herokuapp.com']
 
 
 # Application definition
